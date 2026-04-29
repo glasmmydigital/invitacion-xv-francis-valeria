872fe1cc7c18cafb3dc8a40f2f202c5a4e8671a7
devGlasmmy <glasmmydesarrollo@gmail.com>
2026-04-29 17:12:10 -0600

    Actualización de fotos y textos

--- src/app/pages/des-img/des-img.component.ts
+++ src/app/pages/des-img/des-img.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Inject, PLATFORM_ID } from '@angular/core';
 import { TraductorServicio } from '../../Services/traductor.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-des-img',
@@ -8,9 +9,42 @@ import { TraductorServicio } from '../../Services/traductor.service';
   templateUrl: './des-img.component.html',
   styleUrl: './des-img.component.scss'
 })
-export class DesImgComponent {
-  
-  constructor(public traductorService: TraductorServicio) {
+export class DesImgComponent implements AfterViewInit {
+
+  currentIndexAlt: number = 0;
+
+  constructor(
+    public traductorService: TraductorServicio,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.updateView();
+
+      setInterval(() => {
+        this.shiftSlide(1);
+      }, 3000);
+    }
   }
 
+  updateView(): void {
+    const imageList = document.querySelectorAll('.gallery-track img') as NodeListOf<HTMLImageElement>;
+
+    if (this.currentIndexAlt >= imageList.length) {
+      this.currentIndexAlt = 0;
+    }
+
+    if (this.currentIndexAlt < 0) {
+      this.currentIndexAlt = imageList.length - 1;
+    }
+
+    const track = document.querySelector('.gallery-track') as HTMLElement;
+    track.style.transform = `translateX(-${this.currentIndexAlt * 100}%)`;
+  }
+
+  shiftSlide(direction: number): void {
+    this.currentIndexAlt += direction;
+    this.updateView();
+  }
 }
